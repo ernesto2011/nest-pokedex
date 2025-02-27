d8b1b1b9a327e819870d010b6db5397b5af6002c
ernesto2011 <skillmx107@gmail.com>
2025-02-26 23:45:07 -0600

    ..

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -48,8 +48,21 @@ export class PokemonService {
     return pokemon
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+    const pokemon = await this.findOne(term);
+    if(updatePokemonDto.name){
+      updatePokemonDto.name = updatePokemonDto.name.toLocaleLowerCase().trim();
+    }
+    try {
+      await pokemon.updateOne(updatePokemonDto,{new:true})
+      return {...pokemon.toJSON(), ...updatePokemonDto}
+    } catch (error) {
+      if(error.code === 11000){
+        throw new BadRequestException(`Pokemon already exists ${JSON.stringify(error.keyValue)}`	)
+      }
+      console.log(error)
+      throw new InternalServerErrorException(`Can't create Pokemon - Check server logs`)
+    }
   }
 
   remove(id: number) {
